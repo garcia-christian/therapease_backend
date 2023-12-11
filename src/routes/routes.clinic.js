7760752a5a978b0a3fafab1c576b091b8368db85
christian-garcia <tian.garcia395@gmail.com>
2023-12-12 00:55:08 +0800

    add: get materials

--- src/routes/routes.clinic.js
+++ src/routes/routes.clinic.js
@@ -201,4 +201,35 @@ router.patch("/save-about/:ID", validator, async (req, res) => {
     }
 });
 
+
+router.get("/get-materials/", validator, async (req, res) => {
+
+    try {
+        const materials = await pool.query(`SELECT * FROM public.clinic_materials`);
+
+        res.json(materials.rows);
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Server Error")
+    }
+});
+
+router.post("/add-materials", async (req, res) => {
+    try {
+        const { title, desc, type, file } = req.body;
+
+        const booking = await pool.query(`INSERT INTO public.clinic_materials(
+             "TITLE", "DESC", "TYPE", "FILE")
+            VALUES ($1, $2, $3, $4) RETURNING *;`,
+            [title, desc, type, file])
+        console.log(booking.rows);
+        return res.send(booking.rows[0]);
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send(error.message)
+    }
+});
+
 module.exports = router;
